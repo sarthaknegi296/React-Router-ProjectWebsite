2de9b50b4a3a27e1c2e6da922347649355e78e03
Sarthak Negi <sarthaknegi296@gmail.com>
2023-09-11 17:11:01 +0530

    First Version

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Home from "./components/HomePage/Home"
+
+function App() {
+  
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
